6c3a2480a7e2bd007b16d438c74612699f42d614
VINITHASIVAKUMAR <sivavinitha07@gmail.com>
2016-08-27 15:03:39 +0530

    Create b6.java

---
+++ b6.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+public class b6
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+int a=sc.nextInt();
+if(a%4==0)
+{
+System.out.print("Leap year");
+}
+else
+{
+System.out.print("Non Leap year");
+}
+}
+}
